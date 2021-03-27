b22b307aec1a9e590948bbb168ee3e1c782b8916
Cule219 <culaficstefan@gmail.com>
2021-03-27 12:24:05 -0400

    signup

--- starter-code/app.js
+++ starter-code/app.js
@@ -9,7 +9,7 @@ const logger = require("morgan");
 const path = require("path");
 
 mongoose
-  .connect("mongodb://localhost/starter-code", { useNewUrlParser: true })
+  .connect(process.env.MONGODB_URI, { useNewUrlParser: true })
   .then((x) => {
     console.log(
       `Connected to Mongo! Database name: "${x.connections[0].name}"`
@@ -42,7 +42,7 @@ app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
 // default value for title local
 app.locals.title = "Express - Generated with IronGenerator";
 
-const index = require("./routes/index");
+const index = require("./routes/index.routes");
 app.use("/", index);
 
 // require the routes from module/file
@@ -50,55 +50,7 @@ const celebrityRoutes = require("./routes/celebrity.routes");
 // tell app to use the routes found in the module
 app.use("/", celebrityRoutes);
 
-module.exports = app;
+const authRoutes = require("./routes/auth.routes");
+app.use("/", authRoutes);
 
-// // How would you get the value of queries and params in the following examples?
-// // PARAMS:
-// // 1:
-// // https://dummyexample.com/albums/123cool4lbum
-// app.get("/albums/:albumId", (req, res) => {
-//   console.log(req.params.albumId);
-// });
-// // 2:
-// // https://dummyexample.com/students/nick
-// app.get("/students/:name", (req, res) => {
-//   console.log(req.params.name);
-// });
-// // 3:
-// // https://dummyexample.com/nature/large/1
-// app.get("/:imageType/:imageSize/:id", (req, res) => {
-//   // grab them
-//   const { imageType, imageSize, id } = req.params;
-//   console.log(imageType, imageSize, id);
-//   console.log(...req.params);
-// });
-// // 4:
-// // https://dummyexample.com/stefan/123
-// app.get("/:user/:albumId", (req, res) => {
-//   const { user, albumId } = req.params;
-//   console.log(user, albumId);
-// });
-// // QUERIES:
-// // 5:
-// // URL: https://localhost:3000/?category=coffee
-// app.get("/", (req, res) => {
-//   console.log(req.query.category);
-// });
-// // 6:
-// // URL: https://localhost:3000/marketplace?category=code-review&type=apps
-// app.get("/marketplace", (req, res) => {
-//   // req.query { category: 'code-review', type: 'apps' }
-//   const { category, type } = req.query;
-//   console.log(category, type);
-// });
-// // 7:
-// // URL: https://localhost:3000/search?city=barcelona&accommodation=hotel&transport=public
-// app.get("/search", (req, res) => {
-//   const { city, accomodation, transport } = req.query;
-//   console.log(city, accomodation, transport);
-// });
-// 8: mix
-// // URL: https://localhost:3000/products/toys?prodID=123
-// app.get("/products/:category", (req, res) => {
-//   console.log(req.query.prodID, req.params.category);
-// });
+module.exports = app;
